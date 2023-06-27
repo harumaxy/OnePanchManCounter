d64ca15ad156edccacba5b958cfd026447be61e3
harumaxy <harumaxy@gmail.com>
2023-06-27 10:50:10 +0900

    Initial Commit

---
+++ OnePanchManCounter/OnePanchManCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnePanchManCounterApp.swift
+//  OnePanchManCounter
+//
+//  Created by Masaharu Hosomichi on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct OnePanchManCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
