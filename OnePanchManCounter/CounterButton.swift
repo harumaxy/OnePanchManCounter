b414a0d837aadc633043d3a2d756e6fd2feec20f
harumaxy <harumaxy@gmail.com>
2023-06-27 13:24:54 +0900

    Implement CounterButton View

---
+++ OnePanchManCounter/CounterButton.swift
@@ -0,0 +1,44 @@
+//
+//  CounterButton.swift
+//  OnePanchManCounter
+//
+//  Created by Masaharu Hosomichi on 2023/06/27.
+//
+
+import SwiftUI
+
+struct CounterButton: View {
+  let training : Training
+  var bgColor : UIColor = .black
+  let action : () -> Void
+  
+  
+  var body: some View {
+    ZStack {
+      Color(bgColor)
+      HStack(spacing: 10)
+      {
+        Button(action: action){
+          VStack{
+            Text(training.name)
+              .foregroundColor(.white)
+              .font(.title)
+            Text(String(format: "%03d", training.count))
+              .foregroundColor(.white)
+              .font(.system(size: 60))
+          }.frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity
+          )
+        }
+      }
+    }
+  }
+}
+
+struct CounterButton_Previews: PreviewProvider {
+  static var previews: some View {
+    CounterButton(training: Training(name: "Test", count: 999)) {}
+      .previewLayout(.sizeThatFits)
+  }
+}
